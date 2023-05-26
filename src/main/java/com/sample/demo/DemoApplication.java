34411e6ebb74b0f3fd5938179ed1fe2bd9a9088d
raghuveerameta <raghuveer.ameta@syntagi.healthcare>
2023-05-27 00:42:08 +0530

    sample code done

---
+++ src/main/java/com/sample/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.sample.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public String checkServer() {
+		return "UP AND RUNNING";
+	}
+}
